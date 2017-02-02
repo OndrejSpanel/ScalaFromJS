7d32dafd9ad2f1b3afe380c8631d1039a1fd4ffb
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-02 23:11:18 +0100

    Trying to import the Esprima (not working yet).

---
+++ src/main/scala/Esprima.scala
@@ -0,0 +1,10 @@
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobalScope
+
+@js.native
+@JSGlobalScope
+object Esprima extends js.Object {
+
+  def isOctalDigit(ch: Char): Boolean = js.native
+
+}
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,8 +1,9 @@
+import Esprima._
 /**
   * Created by Ondra on 2.2.2017.
   */
 class MainTest extends org.scalatest.FunSuite {
   test("Basic test") {
-
+    assert(isOctalDigit('0'))
   }
 }
