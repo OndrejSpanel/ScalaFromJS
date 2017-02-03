f0c1093acd42264d9c7e28337699cd9b3c1568f2
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-03 10:10:22 +0100

    Imported tokenize and parse, tokenize working.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
+import Esprima._
+import JsonToString._
 
 object Main extends js.JSApp {
   @JSExport
   def main(): Unit = {
-    println("Doing nothing ...")
+    println("Testing ...")
+    val code = "answer = 42"
+    val tokens = tokenize(code)
+    val parsed = parse(code)
+    println(s"Tokens: ${tokens.json}")
+    println(s"Parsed: ${parsed.json}")
+    println("Done.")
   }
 }
