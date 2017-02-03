e3d7f4038afec9a3f5a7a7cac6a3f737228d3864
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-03 10:20:49 +0100

    Tokenize test passed in a browser.

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,9 +1,12 @@
 import Esprima._
+import JsonToString._
 /**
   * Created by Ondra on 2.2.2017.
   */
 class MainTest extends org.scalatest.FunSuite {
   test("Basic test") {
-    assert(isOctalDigit('0'))
+    val code = "answer = 42"
+    val tokens = tokenize(code)
+    assert(tokens.json == "[{\"type\":\"Identifier\",\"value\":\"answer\"},{\"type\":\"Punctuator\",\"value\":\"=\"},{\"type\":\"Numeric\",\"value\":\"42\"}]")
   }
 }
