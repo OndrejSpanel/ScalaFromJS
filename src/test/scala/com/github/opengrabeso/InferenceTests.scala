eb7cf4cab42236b31de09a3b2b3fa48ae6a9cbf1
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-17 12:46:49 +0100

    Defined var / var inference tests.

--- src/test/scala/com/github/opengrabeso/InferenceTests.scala
+++ src/test/scala/com/github/opengrabeso/InferenceTests.scala
@@ -1,12 +1,36 @@
 package com.github.opengrabeso
 
-import Uglify._
-import UglifyExt._
 import org.scalatest.FunSuite
 import Resources.{getResource => rsc}
 
-import scala.util.{Failure, Success, Try}
+class InferenceTests extends FunSuite with TestUtils {
+  test("Indenting") {
+    val result = Main.convert(rsc("control/indent.js"))
+  }
 
-class InferenceTests extends FunSuite {
+  test("Detect vals") {
+    ConversionCheck(
+      rsc("inference/variables.js"),
+      Seq(
+        "val i = 1",
+        "val d = 1.2",
+        "val i = 1"
+      ),
+      ConversionCheck.standardForbidden ++ Seq(
+        "function"
+      )
+    )
+  }
+
+  test("Infer var type") {
+    ConversionCheck(
+      rsc("inference/variables.js"),
+      Seq(
+        "var ii = 1",
+        "val di = 1.2",
+        "var aa : Any = 1"
+      ), ConversionCheck.standardForbidden
+    )
+  }
 
 }
