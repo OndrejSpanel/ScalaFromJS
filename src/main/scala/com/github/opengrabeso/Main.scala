88a35273a1cb1f3ef471603074ca7eca98aaa7a9
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-19 09:56:06 +0100

    DRY DelayedAction.

--- src/main/scala/com/github/opengrabeso/Main.scala
+++ src/main/scala/com/github/opengrabeso/Main.scala
@@ -16,48 +16,36 @@ object Main extends js.JSApp {
   private lazy val in = dom.document.getElementById("in")
   private lazy val out = dom.document.getElementById("out")
 
-  object DelayedConversion {
+  trait DelayedAction[T] {
+    def action(pars: T): Unit
+
     // when multiple conversions are pending, execute only the one triggered last
     var lastTriggered = Option.empty[Double]
 
-    def trigger(timestamp: Double, delayMs: Int) = {
+    def trigger(timestamp: Double, delayMs: Int, pars: T) = {
       if (lastTriggered.isEmpty || lastTriggered.get < timestamp) {
         lastTriggered = Some(timestamp)
       }
 
       setTimeout(delayMs) {
-        callback(timestamp)
+        callback(timestamp, pars)
       }
     }
 
-    def callback(timestamp: Double): Unit = {
+    def callback(timestamp: Double, pars: T): Unit = {
       if (lastTriggered.contains(timestamp)) {
-        doConversion()
+        action(pars)
       }
     }
   }
 
-  object DelayedOutput {
-    // when multiple conversions are pending, execute only the one triggered last
-    var lastTriggered = Option.empty[Double]
-
-    def trigger(timestamp: Double, delayMs: Int, result: String) = {
-      if (lastTriggered.isEmpty || lastTriggered.get < timestamp) {
-        lastTriggered = Some(timestamp)
-      }
-
-      setTimeout(delayMs) {
-        callback(timestamp, result)
-      }
-    }
-
-    def callback(timestamp: Double, result: String): Unit = {
-      if (lastTriggered.contains(timestamp)) {
-        out.asInstanceOf[js.Dynamic].value = result
-      }
-    }
+  object DelayedConversion extends DelayedAction[Unit] {
+    def action(pars: Unit) = doConversion()
   }
 
+  object DelayedOutput extends DelayedAction[String] {
+    def action(result: String) = out.asInstanceOf[js.Dynamic].value = result
+  }
 
   def convert(code: String): String = {
     val ast = parse(code, defaultUglifyOptions.parse)
@@ -86,12 +74,12 @@ object Main extends js.JSApp {
     val code = in.asInstanceOf[js.Dynamic].value.asInstanceOf[String]
     // longer delay on a long code
     val delayMs = 10 max code.length / 100 min 1000
-    DelayedConversion.trigger(e.timeStamp, delayMs)
+    DelayedConversion.trigger(e.timeStamp, delayMs, ())
   }
 
   private def onPaste(e: Event) = {
     // no delay needed after paste
-    DelayedConversion.trigger(e.timeStamp, 10)
+    DelayedConversion.trigger(e.timeStamp, 10, ())
   }
 
 
