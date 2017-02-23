7cebe16d40db03dd94455b8363f98e14500e70ef
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-23 11:11:33 +0100

    Map js names to Scala names.

--- src/main/scala/com/github/opengrabeso/SymbolTypes.scala
+++ src/main/scala/com/github/opengrabeso/SymbolTypes.scala
@@ -15,11 +15,21 @@ object SymbolTypes {
     token.map(t => SymbolMapId(sym.name, t.pos))
   }
 
+  def mapSimpleTypeToScala(tpe: String): String = {
+    tpe match {
+      case "string" => "String"
+      case "number" => "Double"
+      case "boolean" => "Boolean"
+      case _ => tpe
+    }
+  }
+
   def apply(): SymbolTypes = new SymbolTypes(Map.empty)
   def apply(syms: Seq[(SymbolDef, TypeDesc)]) = {
     val idMap = syms.map { case (k,v) => id(k) -> v }.toMap - None
-    new SymbolTypes(idMap.map{ case (k, v) => k.get -> v})
+    new SymbolTypes(idMap.map{ case (k, v) => k.get -> mapSimpleTypeToScala(v)})
   }
+
 
 }
 
