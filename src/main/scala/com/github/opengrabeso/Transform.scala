ded443653779abc787e1077634a15d8404d9eda9
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-25 21:00:06 +0100

    Replace prototype values with (read only) functions.

--- src/main/scala/com/github/opengrabeso/Transform.scala
+++ src/main/scala/com/github/opengrabeso/Transform.scala
@@ -547,6 +547,8 @@ object Transform {
       node match {
         case ClassMemberDef(name, _, _, _) if classes.get(name).isDefined =>
           new AST_EmptyStatement()
+        case ClassPropertyDef(name, _, _) if classes.get(name).isDefined =>
+          new AST_EmptyStatement()
         case ClassParentDef(name, _) if classes.get(name).isDefined =>
           new AST_EmptyStatement()
         case defun@ClassDefine(sym, _, _) if classes contains sym.name =>
@@ -564,7 +566,7 @@ object Transform {
               //init = this
             }
             // TODO: resolve a symbol
-            `extends` = clazz.base.fold(js.undefined:UndefOr[AST_Node]) { b =>
+            `extends` = clazz.base.fold(js.undefined: UndefOr[AST_Node]) { b =>
               new AST_SymbolRef {
                 /*_*/
                 fillTokens(this, defun) // TODO: tokens from a property instead
@@ -573,19 +575,39 @@ object Transform {
               }
             }
             properties = clazz.members.map { case (k, v) =>
-              new AST_ConciseMethod {
-                // symbol lookup will be needed
-                key = new AST_SymbolRef {
-                  fillTokens(this, defun) // TODO: tokens from a property instead
-                  name = k
-                }
-                value = new AST_Accessor {
-                  fillTokens(this, defun) // TODO: tokens from a property instead
-                  argnames = v.args
-                  this.body = v.body
+              v match {
+                case m: ClassFunMember =>
+                  new AST_ConciseMethod {
+                    // symbol lookup will be needed
+                    key = new AST_SymbolRef {
+                      fillTokens(this, defun) // TODO: tokens from a property instead
+                      name = k
+                    }
+                    value = new AST_Accessor {
+                      fillTokens(this, defun) // TODO: tokens from a property instead
+                      argnames = m.args
+                      this.body = m.body
 
-                }
-              }: AST_ObjectProperty
+                    }
+                  }: AST_ObjectProperty
+                case m: ClassVarMember =>
+                  new AST_ConciseMethod {
+                    // symbol lookup will be needed
+                    key = new AST_SymbolRef {
+                      fillTokens(this, defun) // TODO: tokens from a property instead
+                      name = k
+                    }
+                    value = new AST_Accessor {
+                      fillTokens(this, defun) // TODO: tokens from a property instead
+                      argnames = js.Array()
+                      this.body = js.Array(new AST_SimpleStatement {
+                        fillTokens(this, defun)
+                        body = m.value
+                      })
+
+                    }
+                  }: AST_ObjectProperty
+              }
             }.toJSArray
           }
         case _ =>
@@ -616,7 +638,11 @@ object Transform {
     }
   }
 
-  case class ClassMember(args: js.Array[AST_SymbolFunarg], body: js.Array[AST_Statement])
+  sealed trait ClassMember
+
+  case class ClassFunMember(args: js.Array[AST_SymbolFunarg], body: js.Array[AST_Statement]) extends ClassMember
+
+  case class ClassVarMember(value: AST_Node) extends ClassMember
 
   case class ClassDef(base: Option[String] = None, members: Map[String, ClassMember] = Map.empty)
 
@@ -624,6 +650,14 @@ object Transform {
     def unapply(arg: AST_Node) = arg match {
       case AST_SimpleStatement(AST_Assign(AST_Dot(AST_Dot(AST_SymbolRef(name, _, _), "prototype"), funName), "=", AST_Function(args, body))) =>
         Some(name, funName, args, body)
+      case _ => None
+    }
+  }
+
+  object ClassPropertyDef {
+    def unapply(arg: AST_Node) = arg match {
+      case AST_SimpleStatement(AST_Assign(AST_Dot(AST_Dot(AST_SymbolRef(name, _, _), "prototype"), funName), "=", value)) =>
+        Some(name, funName, value)
       case _ => None
     }
   }
@@ -657,7 +691,7 @@ object Transform {
         if (classNames contains sym.name) {
           // looks like a constructor
           //println("Constructor " + sym.name)
-          val constructor = ClassMember(args, body)
+          val constructor = ClassFunMember(args, body)
           classes += sym.name -> ClassDef(members = Map("constructor" -> constructor))
         }
         true
@@ -665,8 +699,15 @@ object Transform {
       case ClassMemberDef(name, funName, args, body) =>
         //println(s"Assign $name.$funName")
         for (clazz <- classes.get(name)) {
-          val member = ClassMember(args, body)
+          val member = ClassFunMember(args, body)
           classes += name -> clazz.copy(members = clazz.members + (funName -> member))
+        }
+        true
+      case ClassPropertyDef(name, propName, value) =>
+        //println(s"Assign $name.$funName")
+        for (clazz <- classes.get(name)) {
+          val member = ClassVarMember(value)
+          classes += name -> clazz.copy(members = clazz.members + (propName -> member))
         }
         true
       case ClassParentDef(name, AST_New(AST_SymbolRefDef(sym), _*)) =>
