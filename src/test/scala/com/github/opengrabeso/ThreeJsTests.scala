a439f10cbd599dc07a6bd18275db05076dea2d26
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-03-09 09:48:25 +0100

    Added a pending test for Vector3.

---
+++ src/test/scala/com/github/opengrabeso/ThreeJsTests.scala
@@ -0,0 +1,24 @@
+package com.github.opengrabeso
+
+import Resources.{getResource => rsc}
+
+import org.scalatest.FunSuite
+
+class ThreeJsTests extends FunSuite with TestUtils {
+
+  test("Handle Scala keywords in identifiers") {
+    pendingUntilFixed {
+      execute check ConversionCheck(rsc("expressions/reserved.js"))
+        .required(
+          "class Vector3(x: Double = 0, y: Double = 0, z: Double = 0)",
+          "(axis: Vector3, angle: Double)",
+          "def dot(v: Vector3)"
+        )
+        .forbidden(
+          "return (axis, angle)",
+          "break",
+          "console.warn"
+        )
+    }
+  }
+}
