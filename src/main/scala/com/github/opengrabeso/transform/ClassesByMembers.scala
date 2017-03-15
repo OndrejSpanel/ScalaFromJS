a0d57471545321cc2c49bfa4ab9bc3c372833e4e
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-03-15 11:08:59 +0100

    Use function members as well. Use first identifier letter when otherwise equal.

--- src/main/scala/com/github/opengrabeso/transform/ClassesByMembers.scala
+++ src/main/scala/com/github/opengrabeso/transform/ClassesByMembers.scala
@@ -17,7 +17,13 @@ object ClassesByMembers {
   case class ClassDefInfo(members: Set[String], funMembers: Set[String], parentCount: Int)
 
   case class MemberList(classInfo: ClassInfo, types: SymbolTypes) {
-    var list = Map.empty[SymbolMapId, Set[String]]
+
+    case class ClassUseInfo(members: Set[String], funMembers: Set[String]) {
+      def addMember(member: String): ClassUseInfo = copy(members = members + member)
+      def addFunMember(member: String): ClassUseInfo = copy(funMembers = funMembers + member)
+    }
+
+    var list = Map.empty[SymbolMapId, ClassUseInfo]
 
     val defList = {
       var members = Map.empty[String, ClassDefInfo]
@@ -51,25 +57,46 @@ object ClassesByMembers {
     def addMember(sym: SymbolDef, member: String) = {
       for (sid <- id(sym)) {
         //println(s"Add mem $sid $member")
-        val memUpdated = list.get(sid).fold(Set(member))(_ + member)
+        val memUpdated = list.get(sid).fold {
+          ClassUseInfo(Set(member), Set())
+        }(_.addMember(member))
         list += sid -> memUpdated
       }
     }
 
-    def bestMatch(members: Set[String]): Option[String] = {
+    def addFunMember(sym: SymbolDef, member: String) = {
+      for (sid <- id(sym)) {
+        //println(s"Add mem $sid $member")
+        val memUpdated = list.get(sid).fold {
+          ClassUseInfo(Set(), Set(member))
+        }(_.addFunMember(member))
+        list += sid -> memUpdated
+      }
+    }
+
+    def matchNames(use: String, cls: String): Int = {
+      if (use.head.toLower == cls.head.toLower) 1 // prefer a class with the same first lecheck first identifier letter
+      else 0
+    }
+
+    def bestMatch(useName: String, useInfo: ClassUseInfo): Option[String] = {
       defList.headOption.flatMap { _ =>
         val best = defList.map { case (cls, ms) =>
           (
-            (ms.members intersect members).size, // prefer the class having most common members
+            (ms.members intersect useInfo.members).size + (ms.funMembers intersect useInfo.funMembers).size, // prefer the class having most common members
             -ms.members.size, // prefer a smaller class
             -ms.parentCount, // prefer a less derived class
+            matchNames(useName, cls), // prefer a class with a matching name
             cls // keep ordering stable, otherwise each iteration may select a random class
+            //, ms.members intersect useInfo.members, ms.funMembers intersect useInfo.funMembers // debugging
           )
           //println(s"  Score $ms -> $members: $r")
-        }.max
-        //println(s"Best $m for $members")
+        }.max //By(b => (b._1, b._2, b._3, b._4))
         // if there are no common members, do not infer any type
-        if (best._1 > 0) Some(best._4)
+        if (best._1 > 0) {
+          //println(s"$useInfo: Best $best")
+          Some(best._5)
+        }
         else None
       }
     }
@@ -93,13 +120,12 @@ object ClassesByMembers {
       descend(node, walker)
 
       node match {
-        // TODO: accept member access as well, like in SymbolAccess
         case cls@AST_SymbolRefDef(sym) AST_Dot member =>
           val tpe = expressionType(cls)(ctx)
           if (tpe.isEmpty) {
             walker.parent().nonNull match {
               case Some(_: AST_Call) =>
-                // TODO: add function member use
+                byMembers.addFunMember(sym, member)
               case _ =>
                 //println(s"  $tpe.$member -- ${sym.name}")
                 byMembers.addMember(sym, member)
@@ -115,7 +141,7 @@ object ClassesByMembers {
     // for each find the best match
     for {
       (sid, members) <- byMembers.list
-      cls <- byMembers.bestMatch(members)
+      cls <- byMembers.bestMatch(sid.name, members)
     } {
       //println(s"Add type $sid $cls")
       allTypes.t += Some(sid) -> TypeInfo.target(ClassType(cls))
