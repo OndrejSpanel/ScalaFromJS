7b5c397b2ea66c56e02694f1c68ce3a219fc3b5c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-03-16 10:32:09 +0100

    Fix behaviour with no local storage (running from file).

--- src/main/scala/com/github/opengrabeso/Persist.scala
+++ src/main/scala/com/github/opengrabeso/Persist.scala
@@ -5,7 +5,7 @@ import org.scalajs.dom
 object Persist {
   val storage = dom.window.localStorage
 
-  def store(name: String, value: String) = storage.setItem(name, value)
+  def store(name: String, value: String) = if (storage!=null) storage.setItem(name, value)
 
-  def load(name: String): Option[String] = Option(storage.getItem(name))
+  def load(name: String): Option[String] = Option(storage).map(_.getItem(name))
 }
