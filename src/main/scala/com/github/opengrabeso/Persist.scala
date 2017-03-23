63c844111b42628a2f570da5991f6750f9e5454b
Ondřej Španěl <ondrejspanel@users.noreply.github.com>
2017-03-23 22:45:55 +0100

    Flag to disable storage.

--- src/main/scala/com/github/opengrabeso/Persist.scala
+++ src/main/scala/com/github/opengrabeso/Persist.scala
@@ -5,7 +5,9 @@ import org.scalajs.dom
 import scala.util.Try
 
 object Persist {
-  val storage = Try { Option(dom.window.localStorage) }.toOption.flatten
+  val enableStorage = true
+
+  val storage = if (enableStorage) Try { Option(dom.window.localStorage) }.toOption.flatten else None
 
   def store(name: String, value: String) = storage.foreach(_.setItem(name, value))
 
