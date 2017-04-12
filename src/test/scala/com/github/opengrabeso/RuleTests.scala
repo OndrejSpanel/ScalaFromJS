64d83d70194c11998082c20c7241d60be2a2bf3a
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-04-12 16:43:35 +0200

    Add a test for property rules.

--- src/test/scala/com/github/opengrabeso/RuleTests.scala
+++ src/test/scala/com/github/opengrabeso/RuleTests.scala
@@ -12,6 +12,38 @@ class RuleTests extends FunSuite with TestUtils {
         "def exotic",
         "var exotic"
      )
+  }
 
+  test("Replace member variables with properties") {
+    execute check ConversionCheck(
+      //language=JavaScript
+      """
+      function C(width, height) {
+        this.parameters = {
+          width: width,
+          height: height
+        };
+        this.name = "C";
+      }
+
+      C.prototype.constructor = C;
+
+
+      var ScalaFromJS_settings = {
+          members: [
+              {
+                  cls: ".*",
+                  name: "parameters",
+                  operation: "make-property"
+              },{
+                  cls: ".*",
+                  name: "name",
+                  operation: "make-property"
+              }]
+      };
+      """).required(
+        "def parameters()",
+        "def name()"
+      )
   }
 }
