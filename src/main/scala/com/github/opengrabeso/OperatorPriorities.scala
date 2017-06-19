9c9b6213e47e5637312c51b1b6a44b7e9dbecb2e
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-06-19 13:39:43 +0200

    Fixed: comparison priority - parentheses as needed.

--- src/main/scala/com/github/opengrabeso/OperatorPriorities.scala
+++ src/main/scala/com/github/opengrabeso/OperatorPriorities.scala
@@ -6,12 +6,21 @@ object OperatorPriorities {
     else right && nonAssociative.contains(op)
   }
 
+  val assignments = Set("=", "+=", "-=", "*=", "/=", "&=", "|=", "^=", "%=")
+  val dot = Set(Symbols.instanceof, Symbols.asinstanceof)
+  val comparisons = Seq(Set("||"), Set("&&"), Set("==", "!=", "<=", ">=", "<", ">"))
+
   val priorityOrderings = Seq(
-    Seq(Set("+", "-"),Set("*", "/", "%")),
-    Seq(Set("|","^"), Set("&"))
+    Seq(assignments) ++ comparisons ++ Seq(Set("+", "-"),Set("*", "/", "%"), dot),
+    Seq(assignments, Set("|"), Set("^"), Set("&"), dot),
+    Seq(assignments, Set("<<", ">>"), dot)
   )
 
-  val nonAssociative = Set("/", "-")
+  val nonAssociative = Set("/", "-", "%")
+
+  def findOrdering(op: String): Option[Seq[Set[String]]] = {
+    priorityOrderings.find(_.exists(_.contains(op)))
+  }
 
   def needsParens(outer: String, inner: String, right: Boolean): Boolean = {
     //println(s"outer $outer inner $inner")
@@ -22,8 +31,11 @@ object OperatorPriorities {
     def innerIsSamePrio = priorityOrderings.exists { ordering =>
       ordering.exists(o => o.contains(inner) && o.contains(outer))
     }
+    def sameOrdering(op1: String, op2: String) = {
+      priorityOrderings.exists(o => o.exists(_ contains op1) && o.exists(_ contains op2))
+    }
     //println(s"outer $outer inner $inner => $innerIsLowerPrio")
-    innerIsLowerPrio || right && (nonAssociative contains outer) && innerIsSamePrio
+    innerIsLowerPrio || right && (nonAssociative contains outer) && innerIsSamePrio || !sameOrdering(outer, inner)
   }
 
 }
