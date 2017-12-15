8798b546f89d07298c5c0973da72cd8ce7d28661
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-12-15 18:26:26 +0100

    Simplify empty file.

--- src/main/scala/com/github/opengrabeso/esprima/Example.scala
+++ src/main/scala/com/github/opengrabeso/esprima/Example.scala
@@ -1,9 +1,5 @@
 package com.github.opengrabeso.esprima
 
-import scala.reflect.runtime.universe._
-import esprima.Esprima.parse
-import esprima.Node
-import esprima.Node._
 
 object Example extends App {
 
