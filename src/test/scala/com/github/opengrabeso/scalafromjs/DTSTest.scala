2b27918649bff87fb6bb72059e94c670789bd5a0
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-12-16 01:48:39 +0100

    Add more target test conditions.

--- src/test/scala/com/github/opengrabeso/scalafromjs/DTSTest.scala
+++ src/test/scala/com/github/opengrabeso/scalafromjs/DTSTest.scala
@@ -7,7 +7,7 @@ class DTSTest extends FunSuite with TestUtils with ProjectUtils {
     val outCode = convertProject("d.ts/input.js")
     pendingUntilFixed {
       exec check ResultCheck(outCode)
-        .required("r: String")
+        .required("r: String", "bnum: Double", "bstr: String", "xnum: Double")
         .forbidden("x: Double", "x: Any", "x: Unit")
     }
   }
