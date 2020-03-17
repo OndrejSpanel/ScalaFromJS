c7dee85669d57996317e5ece81e4f9453c8ac468
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-03-17 23:07:20 +0100

    Add Web fingerprint

--- js/src/main/scala/com/github/opengrabeso/scalafromjs/GlobalConfig.scala
+++ js/src/main/scala/com/github/opengrabeso/scalafromjs/GlobalConfig.scala
@@ -1,28 +1,14 @@
 package com.github.opengrabeso.scalafromjs
 
+import org.scalajs.dom
+
 object GlobalConfig {
-  lazy val fingerprint = { "TBD"
-    /*
-    // https://stackoverflow.com/a/13546822/16673
-    val p = Convert.getClass.getPackage
-    val name = Option(p.getImplementationTitle).getOrElse("ScalaFromJS")
-    val version = Option(p.getImplementationVersion).getOrElse("Dev")
-    val timestamp = {
-
-      import java.text.SimpleDateFormat
-      import java.util.TimeZone
-      val date = new java.util.Date()
-      val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-
-      // Use Madrid's time zone to format the date in
-      df.setTimeZone(TimeZone.getDefault)
-
-      df.format(date)
-    }
-
-    // Uglify Scala.js version was named like this: ScalaFromJS: 2017-11-22 17:48:10.567
+  lazy val fingerprint = {
+    val p = Convert.getClass.getName //.reverse.dropWhile(_ == '$').reverse
+    val name = p.split('.').dropRight(1).mkString(".")
+    val version = "Web"
+    val timestamp = dom.document.lastModified
     name + ": " + version + " " + timestamp
-    */
   }
 
 }
