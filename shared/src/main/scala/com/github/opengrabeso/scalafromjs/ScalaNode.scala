47398e6054686c09ba9240c99afb7d5794e9a006
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2021-08-30 14:00:34 +0200

    Fix: missing node info copy for custom node types

--- shared/src/main/scala/com/github/opengrabeso/scalafromjs/ScalaNode.scala
+++ shared/src/main/scala/com/github/opengrabeso/scalafromjs/ScalaNode.scala
@@ -8,11 +8,11 @@ object ScalaNode {
   // Scala specific node extensions for the JS AST tree
   case class StatementExpression(var statement: Node.Statement) extends Node.Node with Node.Expression {
 
-    override def clone() = copy()
+    override def clone() = copy().copyNode(this)
   }
 
   case class MemberTemplate(var name: String, var original: Node.ClassBodyElement, var value: String) extends Node.Node with Node.ClassBodyElement {
-    override def clone() = copy()
+    override def clone() = copy().copyNode(this)
   }
 
   walker.allWalkers ++= walker.specializedWalkers(walker.createWalkers[Node.Node, Node.type])
